45c11ad181f7fdc6894c3770725953c8a2ee7b56
Kyle <bossertkt@gmail.com>
2023-08-11 16:47:21 -0700

    Solved.

--- problems/problem_012.py
+++ problems/problem_012.py
@@ -1,12 +1,14 @@
 # Complete the fizzbuzz function to return
 # * The word "fizzbuzz" if number is evenly divisible by
 #   by both 3 and 5
+    #and statements
 # * The word "fizz" if number is evenly divisible by only
 #   3
+    #or statements
 # * The word "buzz" if number is evenly divisible by only
 #   5
 # * The number if it is not evenly divisible by 3 nor 5
-#
+#       else statement
 # Try to combine what you have done in the last two problems
 # from memory.
 
@@ -16,4 +18,17 @@
 # problem to get a good feel for how to solve it.
 
 def fizzbuzz(number):
-    pass
+    if number % 3 == 0 and number % 5 == 0:
+        return "fizzbuzz"
+    elif number % 3 == 0 and number % 5 != 0:
+        return "fizz"
+    elif number % 5 == 0 and number % 3 != 0:
+        return "buzz"
+    else:
+        return number
+
+
+print(fizzbuzz(15))
+print(fizzbuzz(9))
+print(fizzbuzz(20))
+print(fizzbuzz(17))
