691bf21ccb68498d3e7a9d5b4dcfe0c478321005
Kyle <bossertkt@gmail.com>
2023-08-11 12:13:23 -0700

    Solved 001

--- problems/problem_001.py
+++ problems/problem_001.py
@@ -8,5 +8,28 @@
 # Write out some pseudocode before trying to solve the
 # problem to get a good feel for how to solve it.
 
+# def minimum_value(value1, value2):
+#     if value1>value2:
+#         print(value2)
+#     if value1<value2:
+#         print(value1)
+#     if value1 == value2:
+#         print(value1)
+# minimum_value(1, 5)
+
 def minimum_value(value1, value2):
-    pass
+    if value1 > value2:
+        print(value2)
+    elif value1 < value2:
+        print(value1)
+    else:
+        print(value1)
+
+# minimum_value(22, 9)
+
+#given solution
+# def minimum_value(value1, value2):
+#     if value1 < value2:
+#         return value1
+#     else:
+#         return value2
