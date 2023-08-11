1c0aad0a12021feef9548c193b8d5b1b10506859
Kyle <bossertkt@gmail.com>
2023-08-11 12:36:25 -0700

    solved 004

--- problems/problem_004.py
+++ problems/problem_004.py
@@ -12,5 +12,29 @@
 # Write out some pseudocode before trying to solve the
 # problem to get a good feel for how to solve it.
 
+# def max_of_three(value1, value2, value3):
+#     if value1 >= value2 >= value3:
+#         print(value1)
+#     elif value1 <= value2 <= value3:
+#         print(value3)
+#     elif value1 <= value2 >= value3:
+#         print(value2)
+#     elif value1 == value2 > value3:
+#         print(value1)
+#     elif value1 < value2 == value3:
+#         print(value2)
+# max_of_three(6, 2, 3)
+#this doesnt work because i have no condition where
+#  value1 > value2 < value3
+#where value2 is < value3
 def max_of_three(value1, value2, value3):
-    pass
+    if value1 >= value2 and value1 >= value3:
+        print(value1)
+    elif value2 >= value3:
+        print(value2)
+    else:
+        print(value3)
+# just using a bit of logic, with the hint to use "and" statements
+
+
+max_of_three(17, 17.1, 17.02)
