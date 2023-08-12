d7832116cd3eda1f1f33ec75a4b30935d9a39419
Kyle <bossertkt@gmail.com>
2023-08-11 19:28:33 -0700

    Hah that was fun

--- problems/problem_022.py
+++ problems/problem_022.py
@@ -7,4 +7,29 @@
 #     "surfboard"
 
 def gear_for_day(is_workday, is_sunny):
-    pass
+    list_workdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    is_workday = input("What day is it?: ")
+    req_gear = []
+    if is_workday in list_workdays:
+        req_gear.append("laptop")
+    else:
+        req_gear.append("surfboard")
+
+    list_sunny = ["Sunny", "Totally Radical", "pleasant", "just fine", "Not a cloud in the sky"]
+    is_sunny = input("Is it sunny? How's the weather?: ")
+    if is_sunny in list_sunny and is_workday not in list_workdays:
+        req_gear.append("Your snazzy Ray-Bans")
+
+    if is_sunny in list_sunny and is_workday in list_workdays:
+        req_gear.append("workplace appropriate UV-light eye protectors")
+    if is_sunny not in list_sunny and is_workday in list_workdays:
+        req_gear.append("umbrella")
+
+    if is_sunny not in list_sunny and is_workday not in list_workdays:
+        req_gear.append("a wetsuit")
+        # req_gear.append()
+    print("Okay, you need to bring: ")
+    print(req_gear)
+
+
+gear_for_day("", "")
