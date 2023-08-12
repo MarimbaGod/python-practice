3d6b4c15d9a5f057c90a2c64be66639b7d36da2d
Kyle <bossertkt@gmail.com>
2023-08-11 17:35:10 -0700

    Solved

--- problems/problem_016.py
+++ problems/problem_016.py
@@ -3,4 +3,14 @@
 # make sure they're between 0 and 10, inclusive.
 
 def is_inside_bounds(x, y):
-    pass
+    ex_position = 0 <= x <= 10
+    why_position = 0 <= y <= 10
+
+    if ex_position and why_position:
+        return True
+    else:
+        return False
+
+# x = input("x is: ")
+# y = input("y is: ")
+print(is_inside_bounds(x, y))
