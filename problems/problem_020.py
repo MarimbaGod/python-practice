1d243a4dd1124342b927d4de1ab3d435978af3e0
Kyle <bossertkt@gmail.com>
2023-08-11 18:27:55 -0700

    Solved

--- problems/problem_020.py
+++ problems/problem_020.py
@@ -3,4 +3,14 @@
 # to 50% of the number of people in the members list.
 
 def has_quorum(attendees_list, members_list):
-    pass
+    if len(attendees_list) >= (len(members_list) // 2):
+        print(True)
+    else:
+        print(False)
+
+
+
+
+attendees_list = ["Philip", "Gloria"]
+members_list = ["Jason", "Gloria", "McDougal", "Glory", "Gloria"]
+has_quorum(attendees_list, members_list)
