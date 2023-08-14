3c454d7c8c004b9fbe960babe9ed3c6e910ff0c8
Kyle <bossertkt@gmail.com>
2023-08-14 12:00:01 -0700

    starting to get it.

--- problems/problem_070.py
+++ problems/problem_070.py
@@ -19,6 +19,26 @@
 # Do it without pseudocode, this time, from memory. Don't look
 # at the last one you just wrote unless you really must.
 
+class Book:
+    def __init__(self, author_name, title):
+        self.author_name = author_name
+        self.title = title
+
+    # def get_author(self, author_name): #Only need to include
+    # function_name(self): one parameter - (self)
+    #but idk why
+    def get_author(self):
+        # return "Author :" + author_name
+        return "Author: " + self.author_name
+
+    # def get_title(self, title):
+    def get_title(self):
+        # return "Title: " + title # NEED THE self.value_name
+        return "Title: " + self.title
 
 
+#test Example
+book = Book("Sarah J. Maas", "Court of Wings and Ruin")
 
+print(book.get_author())  # prints "Author: Sarah J. Maas"
+print(book.get_title())   # prints "Title: Court of Wings and Ruin"
