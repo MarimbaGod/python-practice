7ec775e87334140114c825e7146140c8d02b08b5
Kyle <bossertkt@gmail.com>
2023-08-14 18:39:36 -0700

    come back to this

--- problems/problem_041.py
+++ problems/problem_041.py
@@ -20,5 +20,42 @@
 
 # Write out your own pseudocode to help guide you.
 
+# input["num,bers", "conn,ected", "to,be,added"]
+#
+#
+#
+#
+#
+#
+#
+#
+#
 def add_csv_lines(csv_lines):
-    pass
+    # csv_lines = ["3,4", "1,9"]
+    result_list = []
+    line_sum = 0
+    for value in csv_lines:
+        bits = value.split(",")
+        # line_sum = 0
+        for bit in bits:
+            # line_sum = 0
+            num = int(bit)
+            line_sum += num
+        result_list.append(line_sum)
+    print(result_list)
+
+csv_lines = ["8,1,7", "10,10,10", "1,2,3"]
+add_csv_lines(csv_lines)
+
+
+# csv_lines = ["3", "1,9"]
+# add_csv_lines(csv_lines)
+
+    #     # str(csv_lines).split(',')
+    #     new_lines.append(str(csv_lines).split(','))
+    #     return new_lines
+
+    # for value1, value2 in new_lines:
+    #     final_lines = []
+    #     final_lines.append((value1 + value2))
+    # print(final_lines)
