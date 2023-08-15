13ae0eb7fe9c0a86d5179f37aa7f5338e968a99f
Kyle <bossertkt@gmail.com>
2023-08-14 19:20:56 -0700

    solved

--- problems/problem_027.py
+++ problems/problem_027.py
@@ -5,4 +5,38 @@
 #
 
 def max_in_list(values):
-    pass
+    # values = [2, 4, 1, 3, 7, 6, 9]
+    if len(values) == 0:
+        return None
+    highest_num = values[0]
+
+    for num in values:
+        if num >= highest_num:
+            highest_num = num
+    return highest_num
+    print(highest_num)
+# print(highest_num)
+
+
+values = [2, -4, 1, 32, 71, 6, 93]
+print(max_in_list(values))
+
+
+
+    # test_num = 0               #closest attempt out of both tries
+    # for num in values:
+    #     test_num =+ num
+    # if
+
+
+#this didnt work because .remove() does not change the index positions
+#         if values[0] >= values[1]:
+#             values.remove(values[1])
+#             # return values[0]
+#         elif values[0] <= values[1]:
+#             values.remove(values[0])
+#         else:
+#             values.remove(values[1])
+#     print(values)
+
+# values = [7, 6, 9]
