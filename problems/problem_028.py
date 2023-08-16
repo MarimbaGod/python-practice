211efdd119f8acb1d04c26ad25b3bef04248e346
Kyle <bossertkt@gmail.com>
2023-08-15 21:43:09 -0700

    it is the exact solution but doesnt test well

--- problems/problem_028.py
+++ problems/problem_028.py
@@ -10,5 +10,33 @@
 #
 # If the list is empty, then return the empty string.
 
-def remove_duplicate_letters(s):
-    pass
+def remove_duplicate_letters(strings):
+    unique_string = ""
+    for letter in strings:
+        if letter not in unique_string:
+            unique_string = unique_string + letter
+    print(unique_string)
+
+
+
+
+#typical.. Made it way more complicated than it needed to be. On the right track though
+
+    # string_letters = []
+
+    # for letter in strings:
+    #     string_letters.append(letter)
+
+    # unique_list = list(set(string_letters))
+    # print(unique_list)
+    # # for letter in strings:
+    #     string_letters.append(letter)
+    # letters = string_letters
+    # unique = []
+    # for letter in letters:
+    #     if letter not in unique:
+    #         unique.insert(0, letter)
+    # print(unique)
+
+strings = ["abccba"]
+remove_duplicate_letters(strings)
---
+++ problems/scratch.py
@@ -0,0 +1,8 @@
+def maximum(numbers):
+    numbers = [1, 2, 3, 4, 5]
+    numbers.sort(reverse=True)
+    max_number = numbers[0]
+    print(numbers[0])
+
+numbers = [1, 2, 3, 4, 5]
+print(maximum(numbers))
