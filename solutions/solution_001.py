b57d85530f56e1ec3de839e0c9c94f1fa2bd11b1
Kyle <bossertkt@gmail.com>
2023-08-18 10:15:32 -0700

    OCD

--- solutions/solution_001.py
+++ solutions/solution_001.py
@@ -1,12 +1,27 @@
-# Complete the minimum_value function so that returns the
-# minimum of two values.
-#
-# If the values are the same, return either.
-#
-# Pseudocode is provided for you to guide you along the way.
-
-def minimum_value(value1, value2):
-    if value1 < value2:
-        return value1
-    else:
-        return value2
+def translate(words, digits):
+    digit_to_char = {
+        "1": "",
+        "2": "ABC",
+        "3": "DEF",
+        "4": "GHI",
+        "5": "JKL",
+        "6": "MNO",
+        "7": "PQRS",
+        "8": "TUV",
+        "9": "WXYZ"
+    }
+​
+    digit_str = str(digits)
+    valid_words_count = 0
+    for word in words:
+        for char_index in range(len(word)):
+            current_char = word[char_index]
+            current_digit = digit_str[char_index]
+            valid_chars = digit_to_char[current_digit]
+​
+            if current_char not in valid_chars:
+                break
+            elif char_index == len(word) - 1:
+                valid_words_count += 1
+​
+    return valid_words_count
