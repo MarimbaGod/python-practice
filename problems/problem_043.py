3d76ebfc3530997f6d80d62e6872d70a8cb3b661
Kyle <bossertkt@gmail.com>
2023-08-20 11:34:57 -0700

    solved

--- problems/problem_043.py
+++ problems/problem_043.py
@@ -18,6 +18,25 @@
 #     result:       [0, 2, 4]
 #
 # Look up the enumerate function to help you with this problem.
+    #for some reason they want this. I Feel like .find() or .index() is better.
+#like I could do index_num = search_list.find(search_term)
+#find couldnt work because i need EVERY occurance of the search_term
+#could do a .remove that occurance..
+
 
 def find_indexes(search_list, search_term):
-    pass
+    # for num in search_list:
+    index_list = []
+    # for index in range(len(search_list) + 1):
+    #     if (search_list[index] == search_term):
+    #         index_list.append(num)
+    # return index_list
+    for i, elem in enumerate(search_list):
+        if elem == search_list:
+            index_list.append(i)
+        return index_list
+
+
+
+# Thanks Bryce
+    # return [i for i, elem in enumerate(search_list) if elem == search_term]
